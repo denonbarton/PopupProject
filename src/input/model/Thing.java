68c2ec3a777f94db3de1a00e57562b5a9e4cb190
denonbarton <dbar0540@740s-pl-126.local>
2015-09-30 13:53:52 -0600

    1

--- src/input/model/Thing.java
+++ src/input/model/Thing.java
@@ -6,13 +6,23 @@ public class Thing
 	private int age;
 	private double weight;
 	
+	/** made to get and set the name, age and weight
+	 * 
+	 */
 	public Thing()
+
 	{
 		age = -99;
-		weight = -.0000005;
+		weight = -.0000009;
 		name = "";
 	}
 	
+	/**
+	 * resets for new user input
+	 * @param name sets user input for "name"
+	 * @param age sets user input for "age"
+	 * @param weight sets user input for "weight"
+	 */
 	public Thing(String name, int age, double weight)
 	{
 		this.age = age;
@@ -20,10 +30,46 @@ public class Thing
 		this.weight = weight;
 	}
 	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+
+	public double getWeight()
+	{
+		return weight;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
+	
+	
 	public String toString()
 
 	{
 		String thingInfo = name + " is " + age + "years old and weighs " + weight;
 		return thingInfo;
+		
+		/**
+		 * finalizes the user input and puts it in its place
+		 */
 	}
 }
+
